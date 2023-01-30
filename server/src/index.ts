9837bb4b9adb1c06d9851948970c8e2dd6cb282c
borsym <borsymatee@gmail.com>
2023-01-30 16:00:01 +0100

    cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,14 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-
+import cors from 'cors';
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 8800;
 
-console.log('hello teszt');
+// middleware
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Seasdrver');
@@ -19,3 +21,4 @@ app.get('/hi', (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
+
