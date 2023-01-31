7c4465d1aa16a97c6462214aff829057d55638ab
borsym <borsymatee@gmail.com>
2023-01-31 20:44:22 +0100

    handle undefined keywords

--- server/src/controllers/gameController.ts
+++ server/src/controllers/gameController.ts
@@ -2,7 +2,11 @@ import { Request, Response } from 'express';
 import axios from 'axios';
 import { baseURL, imgUrl } from '../utils/constants';
 import { ChampionPropertys } from '../utils/interfaces';
-import { extractChampionsData, extractData } from '../utils/utils';
+import {
+  extractChampionsData,
+  extractData,
+  provideOptions,
+} from '../utils/utils';
 import { createDiffieHellmanGroup } from 'crypto';
 import cohere from 'cohere-ai';
 
@@ -18,10 +22,8 @@ export const getGameInfos = async (req: Request, res: Response) => {
 
 export const recommendCharacter = async (req: Request, res: Response) => {
   try {
-    const test = await extracetKeywordsFromPrompt(req.body.prompt);
-    console.log(test);
-    // johet a filter
-
+    const keywords = await extracetKeywordsFromPrompt(req.body.prompt);
+    
     const allCharacter = await axios.get(`${baseURL}/champion.json`);
     const charactersData = allCharacter.data.data;
     const resp = extractData(charactersData);
@@ -42,36 +44,16 @@ export const recommendCharacter = async (req: Request, res: Response) => {
 
 // private
 export const extracetKeywordsFromPrompt = async (prompt: any) => {
-  const options = {
-    method: 'POST',
-    url: 'https://api.cohere.ai/generate',
-    headers: {
-      accept: 'application/json',
-      'Cohere-Version': '2022-12-06',
-      'content-type': 'application/json',
-      authorization: 'Bearer zk2nXqZraOekKKc7BkxZ9WIpTWzFuzQmywe6gl5Y',
-    },
-    data: {
-      model: '327e1dc1-5782-4f3c-8877-bb76e06acea1-ft',
-      truncate: 'END',
-      prompt: prompt,
-      max_tokens: 40,
-      temperature: 0.8,
-      k: 0,
-      p: 0.75,
-    },
-  };
+  const options = provideOptions(prompt);
 
   return axios
     .request(options)
     .then((response: any) => {
       const text = response.data.generations[0].text;
-      const keywords = text
-        .split('\n\n')[1]
-        ?.split('Keywords:')[1]
-        .trim()
-        .split(', ');
-      return keywords;
+      const keywords =
+        text.split('\n\n')[1]?.split('Keywords:')[1].trim().split(', ') ?? [];
+
+      return [...new Set(keywords)];
     })
     .catch((error: any) => {
       console.error(error);
--- server/src/utils/utils.ts
+++ server/src/utils/utils.ts
@@ -19,3 +19,25 @@ export const extractChampionsData = (responses: any) =>
       info: champ.info,
       img: `${imgUrl}/${champ.id}.png`,
     }));
+
+export const provideOptions = (prompt: string) => {
+  return {
+    method: 'POST',
+    url: 'https://api.cohere.ai/generate',
+    headers: {
+      accept: 'application/json',
+      'Cohere-Version': '2022-12-06',
+      'content-type': 'application/json',
+      authorization: 'Bearer zk2nXqZraOekKKc7BkxZ9WIpTWzFuzQmywe6gl5Y',
+    },
+    data: {
+      model: '327e1dc1-5782-4f3c-8877-bb76e06acea1-ft',
+      truncate: 'END',
+      prompt: prompt,
+      max_tokens: 40,
+      temperature: 0.8,
+      k: 0,
+      p: 0.75,
+    },
+  };
+};
