32209dcba9e396273b133c8702013fda3dc9998e
CuriousPercibal <wkzina@inf.elte.hu>
2023-02-02 17:48:05 +0100

    Add keyword listing on champions page, integration with backend

--- compass/src/routes/+page.svelte
+++ compass/src/routes/+page.svelte
@@ -1,18 +1,26 @@
 <script>
     import './styles.css';
-    import Summary from "./utils/Summary.svelte";
     import Champions from "./utils/Champions.svelte";
 
     let response = ""
+    let prompt = ""
 
     async function send() {
-        response = (await fetch("http://localhost:8800/lol/recommend")).json()
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({prompt})
+        }
+        console.log(options);
+        response = (await fetch("http://localhost:8800/api/v1/games/lol/recommend", options)).json()
         console.log(response)
     }
 </script>
 
 <div class="prompt">
-    <textarea placeholder="Write down your desired character..."></textarea>
+    <textarea placeholder="Write down your desired character..." bind:value={prompt}></textarea>
     <div class="button-holder">
         <button class="prompt-button" on:click={send}>Let's go</button>
     </div>
